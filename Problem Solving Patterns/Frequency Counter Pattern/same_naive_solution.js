3d1390359eddb96aec99d93ecf9fb232bab2e5a4
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-19 11:52:40 +0100

    same naive solution - my solution

---
+++ Problem Solving Patterns/Frequency Counter Pattern/same_naive_solution.js
@@ -0,0 +1,18 @@
+function same(arr1, arr2) {
+    // My solution
+    if(arr1.length !== arr2.length) return false;
+
+    arr1 = arr1.sort((a,b) => a - b);
+    arr2 = arr2.sort((a,b) => a - b);
+    
+    console.log('arr1:', arr1, 'arr2:', arr2);
+
+    for(let i = 0; i < arr1.length; i++) {
+        if(arr1[i] ** 2 !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+same([3,6,5,2,8], [25,9,36,64,4]);
