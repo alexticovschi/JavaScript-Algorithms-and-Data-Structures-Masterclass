2132e24674a019bd18a645c5fe2292fbef2e22bc
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-19 11:55:54 +0100

    adding comments

--- Problem Solving Patterns/Frequency Counter Pattern/same_naive_solution.js
+++ Problem Solving Patterns/Frequency Counter Pattern/same_naive_solution.js
@@ -1,3 +1,9 @@
+// Write a function called same, which accepts two arrays.
+// The function should return true if every value in the
+// array has it's corresponding value squared in the second array
+// The frequency of values must be the same.
+
+
 function same(arr1, arr2) {
     // My solution
     if(arr1.length !== arr2.length) return false;
