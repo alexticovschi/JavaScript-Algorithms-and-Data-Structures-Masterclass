a5eab09be36333ce52b22b69b68390fc51777e82
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-23 16:06:17 +0100

    first recursive function

---
+++ Recursion/countdown.js
@@ -0,0 +1,19 @@
+// Recursive Version
+function countDown(num){
+    if(num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+countDown(3)
+
+// Iterative Version
+function countDown(num){
+    for(var i = num; i > 0; i--){
+        console.log(i);
+    }
+    console.log("All done!")
+}
