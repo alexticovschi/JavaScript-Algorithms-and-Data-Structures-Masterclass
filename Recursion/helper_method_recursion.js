53d5ca0f6436d4a9cf9b84528887d640f8a0f1b8
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-23 18:00:05 +0100

    helper method recursion pattern

---
+++ Recursion/helper_method_recursion.js
@@ -0,0 +1,21 @@
+function collectOddValues(arr) {
+    let result = [];
+
+    function helper(helperInput) {
+        if (helperInput.length === 0) {
+            return;
+        }
+
+        if (helperInput[0] % 2 !== 0) {
+            result.push(helperInput[0]);
+        }
+
+        helper(helperInput.slice(1));
+    }
+
+    helper(arr);
+
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7])
